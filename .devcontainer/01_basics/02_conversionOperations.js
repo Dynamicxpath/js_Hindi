a5a13b3c7cf7a0a7c5f00c04a85ffed33778fce8
Girija Shankar Lenka <91020002+Dynamicxpath@users.noreply.github.com>
2023-08-11 08:00:44 +0000

    2nd commit

---
+++ .devcontainer/01_basics/02_conversionOperations.js
@@ -0,0 +1,66 @@
+// let score="33"
+// console.log(typeof score);
+// let valueInNumber= Number(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+
+// let score="33abc"
+// console.log(typeof score);
+// let valueInNumber= Number(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// let score=true
+// console.log(typeof score);
+// let valueInNumber= Number(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// let score="Girija"
+// console.log(typeof score);
+// let valueInNumber= Number(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// let score=null
+// console.log(typeof score);
+// let valueInNumber= Number(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// let score=undefined
+// console.log(typeof score);
+// let valueInNumber= Number(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// let score=1
+// console.log(typeof score);
+// let valueInBoolean= Boolean(score)
+// console.log(valueInBoolean);
+// console.log(typeof valueInBoolean);
+
+// let score=0
+// console.log(typeof score);
+// let valueInBoolean= Boolean(score)
+// console.log(valueInBoolean);
+// console.log(typeof valueInBoolean);
+
+// let score=" "
+// console.log(typeof score);
+// let valueInBoolean= Boolean(score)
+// console.log(valueInBoolean);
+// console.log(typeof valueInBoolean);
+
+// let score=""
+// console.log(typeof score);
+// let valueInBoolean= Boolean(score)
+// console.log(valueInBoolean);
+// console.log(typeof valueInBoolean);
+
+let score=33
+console.log(typeof score);
+let valueInString= String(score)
+console.log(valueInString);
+console.log(typeof valueInString);
