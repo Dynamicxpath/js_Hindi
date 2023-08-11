90d322badb231f4beab715db2405a90448eeef33
Girija Shankar Lenka <91020002+Dynamicxpath@users.noreply.github.com>
2023-08-11 08:04:57 +0000

    4th commit

---
+++ .devcontainer/01_basics/03_comparisonDatatypes.js
@@ -0,0 +1,9 @@
+console.log(undefined == null);
+console.log(undefined == 0);
+console.log(undefined >= null);
+console.log(undefined <= null);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null <= 0);
